de437cfd732639744017c1cf912f941b52267d1f
FerdinandoGeografo <ferdinandogeografo@live.it>
2024-06-29 11:38:57 +0200

    fix

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Users should be able to:
 ### Links
 
 - Solution URL: [https://github.com/FerdinandoGeografo/calculator-app](https://github.com/FerdinandoGeografo/calculator-app)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Live Site URL: [https://calculator-fg.netlify.app/](https://calculator-fg.netlify.app/)
 
 ## My process
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
+  ],
 };
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
